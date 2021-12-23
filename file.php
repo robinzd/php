6e419ec9cc82522a326a777793ea4877f91105c5
robin <ibots.robin@gmail.com>
2021-12-23 20:51:32 +0530

    message

--- file.php
+++ file.php
@@ -38,7 +38,6 @@ mysqli_close($conn);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <style><?php include 'file.css'; ?></style>
     <title>Document</title>
 </head>
 <body>
--- index.php
+++ index.php
@@ -165,7 +165,7 @@ if(isset($_POST['submit'])){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <style><?php include 'index.css'; ?></style>
+    
     <title>Document</title>
 </head>
 <body>
